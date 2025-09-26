47bbe725eac6843689c63733a3e1879aba9cbc7a
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-09-26 18:25:25 +0800

    Add Image Banner

---
+++ revoshop2/src/app/component/banner.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+      <img src="https://im.uniqlo.com/global-cms/spa/res5994e004652b0308c5966b1fa0eba671fr.jpg"/>
+    </div>
+  )
+}
+
+export default Banner
--- revoshop2/src/app/page.tsx
+++ revoshop2/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Banner from "./component/banner";
 
 
 export default function Home() {
   return (
     <div>
       Hello World
+      <Banner></Banner>
     </div>
   );
 }
