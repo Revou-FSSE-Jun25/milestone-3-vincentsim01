05e041680245247bb8f1764fd8e07f712830d6c6
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-10-05 23:31:53 +0800

    domain images

--- revoshop2/next.config.ts
+++ revoshop2/next.config.ts
@@ -3,7 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 
     images: {
+        domains: [
+          'placehold.co',            // placeholder images
+          'api.lorem.space',         // lorem space API
+          'reidosestofados.com.br',  // sofa product images
+          'i.pinimg.com',      
+          'i.imgur.com',
+          'www.google.com',
+          'www.bing.com',
+          'tse3.mm.bing.net',
+          'images.tokopedia.net',
+          'test.url'
+        ],
+
     remotePatterns: [
+      
       {
         protocol: 'https',
         hostname: 'i.imgur.com',
@@ -16,6 +30,33 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placeimg.com',
+        port: '',
+        pathname: '/**',
+      }
+      ,
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      }
+            ,
+      {
+        protocol: 'https',
+        hostname: 'api.lorem.space',
+        port: '',
+        pathname: '/**',
+      }
+
     ],
   },
   /* config options here */
