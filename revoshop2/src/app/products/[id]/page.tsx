289ab25b787b302025247df7958d43ceaf030326
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2025-10-06 00:27:31 +0800

    template product detail

--- revoshop2/src/app/products/[id]/page.tsx
+++ revoshop2/src/app/products/[id]/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 import React from 'react'
+import {getProduct} from '../../lib/api'
 
 const page = async ({ params }: { params: { id: string } }) => {
     const productId = await params.id;
     const isValidId = /^\d+$/.test(productId);
+    // const fetchedData = await getProduct(Number(productId));
+
   return (
     <div className="min-h-screen bg-gray-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
@@ -21,6 +24,15 @@ const page = async ({ params }: { params: { id: string } }) => {
           </ol>
         </nav>
 
+        <div className='m-2 p-5 border border-black shadow-lg rounded-lg flex flex-col items-center'>
+            <h1 className='text-4xl font-bold'>Produt Title</h1>
+            <p>Rp 50.000</p>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Woman%27s_shirt_from_Kutch%2C_Gujarat%2C_India%2C_IMA_55114.jpg/640px-Woman%27s_shirt_from_Kutch%2C_Gujarat%2C_India%2C_IMA_55114.jpg' className='rounded-md border border-black'></img>
+        </div>
+        {/* {fetchedData.title}
+        {fetchedData.price}
+        {fetchedData.description} */}
+        {/* <img src={fetchedData.images[0]}></img> */}
         <div className="mb-6">
           <a
             href="/products"
